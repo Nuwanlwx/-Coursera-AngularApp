13688301a449cd6007925deb784c8f7ed2e7deb3
Nuwan <nuwanlwx@gmail.com>
2020-07-26 21:23:27 +0530

    Assignment2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {LeaderService } from '../services/leader.service';
 
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Leader } from '../shared/leader';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -7,9 +11,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  leaders: Leader[] ;
+  constructor(private leaderService: LeaderService,) { }
 
   ngOnInit() {
+    this.leaders = this.leaderService.getLeaders();
   }
 
 }
