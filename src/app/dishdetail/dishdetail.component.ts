f7229e6cf353354d31f228a8428ffa61749cbb7d
Nuwan <nuwanlwx@gmail.com>
2020-07-30 23:46:07 +0530

    RxJS Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -4,7 +4,7 @@ import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
-
+import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -13,14 +13,17 @@ import { Dish } from '../shared/dish';
 export class DishdetailComponent implements OnInit {
   // @Input()
   dish: Dish;
+  dishIds: string[];
+  prev: string;
+  next: string;
   // selectedDish =  DISHES[0];
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
     private location: Location) { }
 
   ngOnInit() {
-    const id = this.route.snapshot.params['id'];
-     this.dishservice.getDish(id)
+    console.log('dsdsd');
+    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
      .subscribe(dish => this.dish = dish);
   }
   goBack(): void {
